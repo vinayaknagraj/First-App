880455c07f462040c0123aea2dbb49d71426201d
Vinayak Nagaraj <98306561+vinayaknagraj@users.noreply.github.com>
2023-03-01 14:33:43 +0530

    Add files via upload

---
+++ Practice/Practice/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Practice
+//
+//  Created by Vinayak Nagaraj on 21/02/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Vinayak Nagaraj on 21/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textLabel.text = "Hello World"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
